4f8a3061cc123d571cdf5cf4794b8b0c69bab0d8
Ryan Sears <fitblip@gmail.com>
2011-10-19 13:05:25 -0400

    Update irc.py to be multithreaded, changed irclib too, no foreseeable way to not though...

--- python/pybot/irc.py
+++ python/pybot/irc.py
@@ -5,8 +5,8 @@ import irclib, urllib, time, status, java, netsec, signal, sys, drunk, metasploi
 
 network = '66.225.195.39'
 port = 9999
-channel1 = '#testing'
-channel2 = '#testing2'
+channel1 = '#main'
+channel2 = '#whyte'
 nick = 'Zer0-Bot'
 name = 'Zer0 Bot'
 
@@ -112,31 +112,47 @@ class ClientClass ( irclib.SimpleIRCClient ):
         if msg == '!help':
             text = "Heyo " + dude + "! Try !drunk, !status, !status-ticket <ticket>, !javabugs, !netsec, !reddit <cat>, !metasploit <days>, !tv <show>, !buyvm, !ascii, or !sleepytime :-D"
             connection.privmsg(channel,text)
-
 #       def on_privmsg ( self, connection, event ):
 #               connection.
 
 
-#def buyvmstuffs(irc):
-def buyvmstuffs(irc):
-    irc.connection.privmsg('#testing','I ARE TESTINGING. DERPPPP')
-    h = urllib.urlopen('http://doesbuyvmhavestock.com/automation.xml').read()
-    newmd5 = md5.new(h).hexdigest()
-    if oldmd5 == "":
-        print "New run!"
-        print "MD5 : " + newmd5
-        oldmd5 = newmd5
-    if oldmd5 != newmd5:
-        irc.ion.privmsg('#whyte','New BuyVM! Run !buyvm!!!')
-        oldmd5 = newmd5
-    else:
-        print "No MD5 change!"
+# TODO Fix this shit to just be a caller/callee relationship
+# Also, holy hackjob batman!
+def buyvmstuffs(oldmd5,delay):
+    newtime = time.time() + delay
+    print newtime
+    while(1):
+        irc.start()
+        if time.time() >= newtime:
+            h = urllib.urlopen('http://doesbuyvmhavestock.com/automation.xml').read()
+            newmd5 = md5.new(h).hexdigest()
 
+            if oldmd5 == "":
+                print "New run!"
+#                irc.connection.privmsg('#testing','First run!')
+#                irc.connection.privmsg('#testing','MD5 : ' + newmd5)
+                print "MD5 : " + newmd5
+                oldmd5 = newmd5
+            if oldmd5 != newmd5:
+                #irc.connection.privmsg('#testing','MD5 changed! Old MD5 : ' + oldmd5 + '. New MD5 : ' + newmd5)
+                irc.connection.privmsg('#main','New BuyVM stuffs! ZOMG!!!')
+                for msg in buyvm.getinfo():
+                    irc.connection.privmsg('#main',msg)
+                #irc.ion.privmsg('#whyte','New BuyVM! Run !buyvm!!!')
+                oldmd5 = newmd5
+            else:
+                print "No MD5 change!"
+#                irc.connection.privmsg('#testing','No MD5 change. Old MD5 : ' + oldmd5 + '. New MD5 : ' + newmd5)
+
+            newtime = time.time() + delay
+
+#        irc.ircobj.execute_delayed(10,buyvmstuffs(oldmd5))
 
 if __name__=="__main__":
+    oldmd5 = ""
+    delay = 900
     irc = ClientClass()
     irc.connect(network, port, nick, ircname = name, ssl = True)
-    irc.ircobj.execute_delayed(delay=2,function=buyvmstuffs,arguments=(irc))
     irc.connection.join(channel1)
     irc.connection.join(channel2)
-    irc.start()
+    irc.ircobj.execute_delayed(0,buyvmstuffs(oldmd5,delay))
