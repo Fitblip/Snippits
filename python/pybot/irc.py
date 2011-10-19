49dddde99d60f68b0cce972acb60e8838dbfdc26
Ryan Sears <fitblip@gmail.com>
2011-10-19 15:13:02 -0400

    Updated irc.py to not be retarded, and run multiple functions, also included modified version of irclib.py

--- python/pybot/irc.py
+++ python/pybot/irc.py
@@ -2,11 +2,18 @@
 
 import irclib, urllib, time, status, java, netsec, signal, sys, drunk, metasploit, reddit, tvdb, sleepy, buyvm, txtfiles, thread, md5
 
-
 network = '66.225.195.39'
 port = 9999
+
 channel1 = '#main'
+#channel1 = '#testing'
+
 channel2 = '#whyte'
+#channel2 = '#testing2'
+
+delay = 900
+#delay = 5
+
 nick = 'Zer0-Bot'
 name = 'Zer0 Bot'
 
@@ -60,7 +67,7 @@ class ClientClass ( irclib.SimpleIRCClient ):
         if '!tv' in msg:
             show = "".join(str(i + " ") for i in msg.split(' ')[1:])
             message = tvdb.getinfo(show)
-            if message[0] == "ERROR":
+            if message == "ERROR":
                 text = "Doh! There was an error, maybe thetvdb is down?"
                 connection.privmsg(channel,text)
             elif message == "NOTFOUND":
@@ -112,47 +119,41 @@ class ClientClass ( irclib.SimpleIRCClient ):
         if msg == '!help':
             text = "Heyo " + dude + "! Try !drunk, !status, !status-ticket <ticket>, !javabugs, !netsec, !reddit <cat>, !metasploit <days>, !tv <show>, !buyvm, !ascii, or !sleepytime :-D"
             connection.privmsg(channel,text)
-#       def on_privmsg ( self, connection, event ):
-#               connection.
 
-
-# TODO Fix this shit to just be a caller/callee relationship
-# Also, holy hackjob batman!
-def buyvmstuffs(oldmd5,delay):
+def allthethings(delay):
+    # Calculate time + delay
     newtime = time.time() + delay
-    print newtime
-    while(1):
+    # List to pass to buyvm call
+    d = {}
+    while (1):
+        # Retardo modification to irclib to make it break and re-call this to add non-sync 'multi-threading'
         irc.start()
+        # if delay is up
         if time.time() >= newtime:
-            h = urllib.urlopen('http://doesbuyvmhavestock.com/automation.xml').read()
-            newmd5 = md5.new(h).hexdigest()
+            #
+            # Any functions to be run intermittently
+            #
 
-            if oldmd5 == "":
-                print "New run!"
-#                irc.connection.privmsg('#testing','First run!')
-#                irc.connection.privmsg('#testing','MD5 : ' + newmd5)
-                print "MD5 : " + newmd5
-                oldmd5 = newmd5
-            if oldmd5 != newmd5:
-                #irc.connection.privmsg('#testing','MD5 changed! Old MD5 : ' + oldmd5 + '. New MD5 : ' + newmd5)
-                irc.connection.privmsg('#main','New BuyVM stuffs! ZOMG!!!')
-                for msg in buyvm.getinfo():
-                    irc.connection.privmsg('#main',msg)
-                #irc.ion.privmsg('#whyte','New BuyVM! Run !buyvm!!!')
-                oldmd5 = newmd5
+            # Check buyvm for new stock 
+            print "Checking on BuyVM"
+            if len(d.keys()) == 0:
+                print "First run, getting baseline"
+                # First run
+                d = buyvm.getbaseline()
             else:
-                print "No MD5 change!"
-#                irc.connection.privmsg('#testing','No MD5 change. Old MD5 : ' + oldmd5 + '. New MD5 : ' + newmd5)
-
+                print "Not first run, updating and getting a changelist!"
+                change,d = buyvm.checkupdate(d)
+                if len(change) != 0:
+                    irc.connection.privmsg(channel1,'New BuyVM stuffs! ZOMG!!!')
+                    for msg in buyvm.getinfo(change):
+                        irc.connection.privmsg(channel1,msg)
+            
             newtime = time.time() + delay
 
-#        irc.ircobj.execute_delayed(10,buyvmstuffs(oldmd5))
 
 if __name__=="__main__":
-    oldmd5 = ""
-    delay = 900
     irc = ClientClass()
     irc.connect(network, port, nick, ircname = name, ssl = True)
     irc.connection.join(channel1)
     irc.connection.join(channel2)
-    irc.ircobj.execute_delayed(0,buyvmstuffs(oldmd5,delay))
+    irc.ircobj.execute_delayed(0,allthethings(delay))
